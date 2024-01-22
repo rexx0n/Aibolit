2c5e1c8af071ff27c7286bf1d1d7fe9828b08737
rexx0n <alexey.mironenko46@gmail.com>
2024-01-22 14:35:11 +0300

    made block, made carousel

--- js/main.js
+++ js/main.js
@@ -5,3 +5,17 @@ import {functionModal} from "./modal.js";
 madeTargetActive()
 functionModal()
 
+let cards = document.querySelector('.history__cards')
+let btnLeft = document.querySelector('.history__left')
+let btnRight = document.querySelector('.history__right')
+let translate
+let count = 0
+
+btnRight.addEventListener('click', () => {
+    translate = -1158
+    cards.style.transform = `translate(${translate}px) `
+})
+btnLeft.addEventListener('click', () => {
+    translate = 0
+    cards.style.transform = `translate(${translate}px)`
+})
