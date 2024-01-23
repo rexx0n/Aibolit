1b5bbb765f08a9aba640b13aede81ca86e33ec6c
rexx0n <alexey.mironenko46@gmail.com>
2024-01-23 16:06:21 +0300

    made first page

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ let cards = document.querySelector('.history__cards')
 let btnLeft = document.querySelector('.history__left')
 let btnRight = document.querySelector('.history__right')
 let translate
-let count = 0
 
 btnRight.addEventListener('click', () => {
     translate = -1158
