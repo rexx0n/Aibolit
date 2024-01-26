0184b3dcf720eca8799b25beaf3e716cfed5d24f
rexx0n <alexey.mironenko46@gmail.com>
2024-01-26 22:45:34 +0300

    work with the page functionality

--- js/main.js
+++ js/main.js
@@ -12,14 +12,19 @@ catch (err) {
     console.log(err)
 }
 
-
-
-
-
-
 let btns = document.querySelectorAll('.main__info button')
 btns.forEach((btn)=> {
     btn.addEventListener('click', ()=>{
         window.location.href = '../entry.html'
     })
+})
+
+let entryPayBtn = document.querySelector('.entry__pay button')
+let entryButton = document.querySelector('.entry__form form > button')
+let entryPay = document.querySelector('.entry__pay')
+console.log(entryButton)
+entryPayBtn.addEventListener('click', (event)=> {
+    event.preventDefault()
+    entryPay.style.display = 'none'
+    entryButton.classList.remove('entry--none')
 })
